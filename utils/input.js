6d731d213b4daa4934d5de138147ea90438f1b73
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-02-06 14:18:45 -0700

    join roles to the table

--- utils/input.js
+++ utils/input.js
@@ -103,7 +103,30 @@ const input = {
             });
         });
     },
-          
+    viewRoles: function () {
+        return new Promise(function (resolve, reject) {
+          const queryString =
+            "SELECT roles.id, title, salary, name FROM roles LEFT JOIN departments ON roles.department_id = departments.id";
+          connection.query(queryString, function (err, result) {
+                if (err) {
+                return reject(err);
+                }
+                const newTable = [];
+                for (let i = 0; i < result.length; i++) {
+                    const roleObj = {
+                        ID: result[i].id,
+                        Title: result[i].title,
+                        Salary: result[i].salary,
+                        Department: result[i].name,
+                    };
+                    newTable.push(roleObj);
+                }
+                console.table(newTable);
+                return resolve();
+            });
+        });
+    },
+            
 };
 
 module.exports = input;
