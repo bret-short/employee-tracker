3571f000e7c862d3ab837bfc0f2d8ead7ff3c652
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-02-06 14:09:37 -0700

    adding input add department function

--- utils/input.js
+++ utils/input.js
@@ -1,19 +1,20 @@
-const mysql = require("mysql2");
+const connection = require("./connection.js");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
-    user: "root",
-    password: "root",
-    database: "tracker_db"
-});
+const input = {
+  addDepartment: function (deptName) {
+        return new Promise(function (resolve, reject) {
+        const queryString = `INSERT INTO departments (name) VALUES (?)`;
+        connection.query(queryString, deptName, function (err, result) {
+                if (err) {
+                return reject(err);
+                }
+                console.log("Department successfully added!");
+                    return resolve();
+            });
+        });
+    },
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+  
+};
 
-module.exports = connection;
+module.exports = input;
